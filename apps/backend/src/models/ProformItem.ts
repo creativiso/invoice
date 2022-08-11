d60755bff319322d9477f425ac4f0a98d23ea9ca
ViktorKartalov <vkartalcho@gmail.com>
2022-08-11 15:42:18 +0300

    Created sequelize models

---
+++ apps/backend/src/models/ProformItem.ts
@@ -0,0 +1,60 @@
+import {
+    DECIMAL,
+    Model,
+    Optional
+  } from 'sequelize';
+  
+  type ProformItemAttributes = {
+    id: number;
+    proform: number;
+    name: string;
+    quantity: number;
+    measurement: string;
+    price: number;
+  }
+  
+  type ProformItemCreationAttributes = Optional<ProformItemAttributes, 'id'>;
+  
+  module.exports = (sequelize, DataTypes) => {
+    class ProformItem extends Model<ProformItemAttributes, ProformItemCreationAttributes>{
+        declare id: number;
+        declare proform: number;
+        declare name: string;
+        declare quantity: number;
+        declare measurement: string;
+        declare price: number;
+    }
+    ProformItem.init({
+        id: {
+            type: DataTypes.INTEGER(10),
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        proform: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.DECIMAL(12, 4),
+            allowNull: false
+        },
+        measurement: {
+            type: DataTypes.STRING(255),
+            defaultValue: null
+        },
+        price: {
+            type: DECIMAL(13, 4),
+            allowNull: false
+        }
+    },
+    {
+      sequelize,
+      modelName: 'ProformItem',
+    });
+    return ProformItem;
+  };
