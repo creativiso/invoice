9c3e32ef8b78052301dd5a2209d35f0ddde909b3
DenisZhelev <Deniszhelev09@gmail.com>
2023-01-07 16:05:26 +0200

    Make nav-bar responsive

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild} from '@angular/core';
+import {
+  BreakpointObserver,
+  BreakpointState,
+} from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'crtvs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Pages';
-  opened = this.isLargeScreen();
-  isLargeScreen():boolean{
-    if (window.screen.width >728) {
-      return true
-    }
-    return false
+  title = 'frontend';
+  @ViewChild('drawer') drawer: any;
+ public selectedItem  = '';
+  public isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(['(max-width: 1024px)'])
+    .pipe(map((result: BreakpointState) => result.matches));
+
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+closeSideNav() {
+  if (this.drawer._mode=='over') {
+    this.drawer.close();
   }
 }
+}
+//export class AppComponent {
+ 
+  //opened = true;
+  //isLargeScreen():boolean{
+  // if (window.screen.width >1024) {
+  //   return true
+  //  }
+  //  return false
+ //}
+//}
