1773864190c6705db07964d4db40d582ac35ccdf
DenisZhelev <Deniszhelev09@gmail.com>
2023-01-11 15:17:09 +0200

    Responsive side navbar

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,29 +3,32 @@ import {
   BreakpointObserver,
   BreakpointState,
 } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'crtvs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  public isHandset: boolean ; 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @ViewChild('drawer') drawer:any;
- public selectedItem  = '';
-  public isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(['(max-width: 1024px)'])
-    .pipe(map((result: BreakpointState) => result.matches));
-
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
-closeSideNav() {
-  if (this.drawer._mode=='over') {
-    this.drawer.close();
+  @ViewChild('drawer') drawer: any;
+  title = 'frontend';
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isHandset = true;
+    this.breakpointObserver
+      .observe(['(max-width: 1024px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.isHandset = true;
+        } else {
+          this.isHandset = false;
+        }
+      });
   }
-}
-}
 
+  public toggleNav() {
+    this.isHandset = !this.isHandset;
+  }
+  
+}
