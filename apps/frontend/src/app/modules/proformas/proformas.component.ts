ac4291cc7ae18513b65aa43b83b555f7f7ad5272
Medina Ahmed <meditotrampova@gmail.com>
2023-01-15 22:16:19 +0200

    adding receiver and supplier

--- apps/frontend/src/app/modules/proformas/proformas.component.ts
+++ apps/frontend/src/app/modules/proformas/proformas.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'crtvs-proformas',
   templateUrl: './proformas.component.html',
-  styleUrls: ['./proformas.component.css']
+  styleUrls: ['./proformas.component.css'],
 })
-export class ProformasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProformasComponent {
+  proformasForm: FormGroup;
+  individualPerson = new FormControl(false);
+  selectedDate: Date;
+  constructor(private formBuilder: FormBuilder) {
+    this.proformasForm = this.formBuilder.group({
+      supplierName: [''],
+      supplierEik: [''],
+      supplierVatNumber: [''],
+      supplierManager: [],
+      supplierCity: [''],
+      supplierAddress: [''],
+      receiverName: [''],
+      individualPerson: [false],
+      receiverEik: [''],
+      receiverVatNumber: [''],
+      receiverManager: [],
+      receiverCity: [''],
+      receiverAddress: [''],
+      selectedDate: Date,
+    });
   }
 
+  submit() {
+    console.log(this.proformasForm.value);
+  }
 }
--- apps/frontend/src/app/modules/proformas/proformas.module.ts
+++ apps/frontend/src/app/modules/proformas/proformas.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ProformasComponent } from './proformas.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
-
-const routes: Routes = [
-  { path: '', component: ProformasComponent }
-];
+const routes: Routes = [{ path: '', component: ProformasComponent }];
 
 @NgModule({
-  declarations: [
-    ProformasComponent
-  ],
+  declarations: [ProformasComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    RouterModule.forChild(routes),
+  ],
 })
-export class ProformasModule { }
+export class ProformasModule {}
