475a909da311389dcfa0def5b890db8a9796f6b1
Medina Ahmed <meditotrampova@gmail.com>
2023-01-25 08:29:55 +0200

    adding responsive design

--- apps/frontend/src/app/modules/proformas/proformas.component.ts
+++ apps/frontend/src/app/modules/proformas/proformas.component.ts
@@ -1,42 +1,11 @@
 import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
 import {
   FormGroup,
   FormBuilder,
   FormControl,
   Validators,
-  FormArray,
 } from '@angular/forms';
-export interface tableElement {
-  index: number;
-  nameField: string;
-  position: number;
-  qty: number;
-  measure: string;
-  unitPrice: number;
-  amount: number;
-}
 
-const ELEMENT_DATA: tableElement[] = [
-  {
-    index: 0,
-    position: 1,
-    nameField: '',
-    qty: 1,
-    measure: '',
-    unitPrice: 0,
-    amount: 0,
-  },
-  {
-    index: 1,
-    position: 1,
-    nameField: '',
-    qty: 1.0079,
-    measure: '',
-    unitPrice: 0,
-    amount: 0,
-  },
-];
 @Component({
   selector: 'crtvs-proformas',
   templateUrl: './proformas.component.html',
@@ -46,8 +15,21 @@ export class ProformasComponent {
   proformasForm: FormGroup;
   selectedMeasure = '';
   selectedCurrency = '';
-  dataSource = new MatTableDataSource<tableElement>(ELEMENT_DATA);
-  tableData: FormArray;
+  tableData: {
+    nameField: string;
+    quantity: number;
+    measure: string;
+    priceWithoutVat: number;
+    value: number;
+  }[] = [
+    {
+      nameField: '',
+      quantity: 0,
+      measure: '',
+      priceWithoutVat: 0.0,
+      value: 0.0,
+    },
+  ];
 
   constructor(private formBuilder: FormBuilder) {
     this.proformasForm = new FormGroup({
@@ -64,58 +46,36 @@ export class ProformasComponent {
       receiverManager: new FormControl('', Validators.required),
       receiverCity: new FormControl('', Validators.required),
       receiverAddress: new FormControl('', Validators.required),
-      releasedAt: new FormControl(''), //datepicker
-      currency: new FormControl(''),
-      tableData: this.formBuilder.array([
-        this.formBuilder.group({
-          nameField: new FormControl('', Validators.required),
-          position: new FormControl('', Validators.required),
-          qty: new FormControl('', Validators.required),
-          measure: new FormControl('', Validators.required),
-          unitPrice: new FormControl('', Validators.required),
-          amount: new FormControl('', Validators.required),
-        }),
-      ]),
+      releasedAt: new FormControl('', Validators.required), //datepicker
+      currency: new FormControl('', Validators.required),
+      nameField: new FormControl('', Validators.required),
+      quantity: new FormControl('', Validators.required),
+      measure: new FormControl(''),
+      priceWithoutVat: new FormControl('', Validators.required),
       vatPercent: new FormControl('', Validators.required),
-      vatReason: new FormControl('', Validators.required),
+      vatReason: new FormControl(''),
       wayOfPaying: new FormControl('', Validators.required),
-      iban: new FormControl('', Validators.required),
-      bicSwift: new FormControl('', Validators.required),
-      bankName: new FormControl('', Validators.required),
     });
-    this.tableData = this.proformasForm.get('tableData') as FormArray;
-    this.addRow();
   }
-  displayedColumns: string[] = [
-    'position',
-    'nameField',
-    'qty',
-    'measure',
-    'unitPrice',
-    'amount',
-  ];
-
   addRow() {
-    const newRow: tableElement = {
-      index: this.dataSource.data.length,
-      position: 0,
+    const newRow = {
       nameField: '',
-      qty: 0,
+      quantity: 0,
       measure: '',
-      unitPrice: 0,
-      amount: 0,
+      priceWithoutVat: 0.0,
+      value: 0.0,
     };
-    this.dataSource.data.push(newRow);
-    this.dataSource._updateChangeSubscription();
+    this.tableData.push(newRow);
   }
-
-  deleteRow(element: tableElement) {
-    const index = this.dataSource.data.indexOf(element);
-    this.dataSource.data.splice(index, 1);
-    this.dataSource._updateChangeSubscription();
+  deleteRow(index: number) {
+    if (this.tableData.length > 1) {
+      this.tableData.splice(index, 1);
+    } else {
+      // code to show an error message or alert
+      console.log('You should have at least one row');
+    }
   }
   onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(this.proformasForm.value);
+    //
   }
 }
