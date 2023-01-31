570820cac1f6c26eb4ae59bd3140c56eee5ebf7a
Medina Ahmed <meditotrampova@gmail.com>
2023-01-31 13:04:58 +0200

    add formControls

--- apps/frontend/src/app/modules/proformas/proformas.component.ts
+++ apps/frontend/src/app/modules/proformas/proformas.component.ts
@@ -4,6 +4,7 @@ import {
   FormBuilder,
   FormControl,
   Validators,
+  FormArray,
 } from '@angular/forms';
 interface SelectedMeasure {
   [id: number]: string;
@@ -17,9 +18,12 @@ export class ProformasComponent implements OnInit {
   proformasForm: FormGroup;
   selectedMeasure: SelectedMeasure = {};
   selectedCurrency = '';
-  quantity = 0.0;
-  priceWithoutVat = 0.0;
+  quantity = 0;
+  priceWithoutVat = 0;
   vatPercent = 0;
+  formArray: FormArray;
+  public rowAmount = 0;
+  public totalAmount = 0;
 
   tableData: {
     nameField: string;
@@ -36,7 +40,11 @@ export class ProformasComponent implements OnInit {
       value: 0.0,
     },
   ];
-
+  // data = {
+  //   priceWithoutVat: 0,
+  //   quantity: 0,
+  //   vatPercent: 0,
+  // };
   constructor(private formBuilder: FormBuilder) {
     this.proformasForm = new FormGroup({
       supplierName: new FormControl('', Validators.required),
@@ -54,54 +62,61 @@ export class ProformasComponent implements OnInit {
       receiverAddress: new FormControl('', Validators.required),
       releasedAt: new FormControl('', Validators.required), //datepicker
       currency: new FormControl(''),
-      nameField: new FormControl('', Validators.required),
-      quantity: new FormControl('', Validators.required),
-      measure: new FormControl(''),
-      priceWithoutVat: new FormControl('', Validators.required),
-      vatPercent: new FormControl('', Validators.required),
+
+      wayOfPaying: new FormControl(''),
       vatReason: new FormControl(''),
-      wayOfPaying: new FormControl('', Validators.required),
+    });
+
+    //this.formArray = new FormArray([this.createRow()]);
+    this.formArray = this.formBuilder.array([this.createRow()]);
+    this.proformasForm.addControl('tableData', this.formArray);
+  }
+
+  createRow(): FormGroup {
+    return this.formBuilder.group({
+      tableInfo: this.formBuilder.group({
+        nameField: ['', Validators.required],
+        quantity: ['', Validators.required],
+        measure: [''],
+        priceWithoutVat: ['', Validators.required],
+        vatPercent: ['', Validators.required],
+      }),
     });
   }
+
   ngOnInit() {
-    this.proformasForm.get('vatPercent')?.valueChanges.subscribe((value) => {
-      this.vatPercent = value;
+    const tableData = <FormArray>this.proformasForm.get('tableData');
+    tableData.valueChanges.subscribe(() => {
+      this.tableData = tableData.value;
     });
-    this.proformasForm.get('quantity')?.valueChanges.subscribe((value) => {
-      this.quantity = value;
-    });
-    this.proformasForm
-      .get('priceWithoutVat')
-      ?.valueChanges.subscribe((value) => {
-        this.priceWithoutVat = value;
-      });
-  }
-  //geting the sum of quntity and unitPrice
-  get amount(): number {
-    return this.quantity * this.priceWithoutVat;
-  }
-  get totalAmount(): number {
-    return (this.amount * this.vatPercent) / 100 + this.amount;
   }
   addRow() {
-    const newRow = {
-      nameField: '',
-      quantity: 0,
-      measure: '',
-      priceWithoutVat: 0.0,
-      value: 0.0,
-    };
-    this.tableData.push(newRow);
+    (<FormArray>this.proformasForm.get('tableData')).push(this.createRow());
     this.selectedMeasure[this.tableData.length - 1] = '';
   }
   deleteRow(index: number) {
     if (this.tableData.length > 1) {
-      this.tableData.splice(index, 1);
-    } else {
-      // code to show an error message or alert
-      console.log('You should have at least one row');
+      (this.proformasForm.get('tableData') as FormArray).removeAt(index);
     }
   }
+  calculateRowAmount(): number {
+    {
+      this.rowAmount = this.quantity * this.priceWithoutVat;
+      return this.rowAmount;
+    }
+  }
+  calculateTotalAmount(): number {
+    this.formArray.controls.forEach((formGroup) => {
+      //const quantityControl = formGroup.get('quantity');
+      // const priceWithoutVatControl = formGroup.get('priceWithoutVat');
+      // this.rowAmount = quantityControl?.value * priceWithoutVatControl?.value;
+      // const vatPercent = formGroup.get('vatPercent');
+      //  this.totalAmount = (this.rowAmount * 20) / 100 + this.rowAmount; // instead of 20 use vatpercent
+    });
+
+    return this.totalAmount;
+  }
+
   onSubmit() {
     //
   }
