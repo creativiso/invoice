fa381da09b5d6bbd5bc20745e7c79fa6e6f4df54
DimitarKushev <102426559+DimitarKushev@users.noreply.github.com>
2023-01-31 08:41:44 +0200

    Contractor form (#9)
    * Contractor form
    
    Create contractor form
    
    * Contractors form
    
    Create form for Contractors.

--- apps/frontend/src/app/modules/contractors/contractors.module.ts
+++ apps/frontend/src/app/modules/contractors/contractors.module.ts
@@ -2,19 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ContractorsComponent } from './contractors.component';
-
-
-const routes: Routes = [
-  { path: '', component: ContractorsComponent }
-];
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+const routes: Routes = [{ path: '', component: ContractorsComponent }];
 
 @NgModule({
-  declarations: [
-    ContractorsComponent
-  ],
+  declarations: [ContractorsComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    RouterModule.forChild(routes),
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+  ],
 })
-export class ContractorsModule { }
+export class ContractorsModule {}
