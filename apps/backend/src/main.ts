b3bfb40cc5d715dbc5e71f5153beb3dbe9661a55
DenisZhelev <Deniszhelev09@gmail.com>
2023-02-01 15:16:45 +0200

    Updated models

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,12 +6,12 @@ const app = express();
 const apiRouter = Router();
 app.use('/api/v1', apiRouter);
 
+(async () => {
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
-(async () => {
   try {
     await sequelize.sync();
     console.log('Database has been initialized.');
