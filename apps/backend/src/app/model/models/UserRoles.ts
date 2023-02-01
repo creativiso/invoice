877547e78f6c12033bb9ac2572426bc2324e0e22
DenisZhelev <119076896+DenisZhelev@users.noreply.github.com>
2023-02-01 15:20:18 +0200

    Ci 16 (#10)
    * model Changes and adding interface
    
    * Added interfaces for the Morels
    
    * uodated models
    
    * updated models
    
    * updated models
    
    * updated models
    
    * updated models
    
    * updated
    
    * Updated Models
    
    * Updated models
    
    * Updated Models
    
    * Updated Models

---
+++ apps/backend/src/app/model/models/UserRoles.ts
@@ -0,0 +1,19 @@
+import { Table, Column, Model, ForeignKey } from 'sequelize-typescript';
+import { DataTypes } from 'sequelize';
+import { IUserRoles } from 'libs/typings/src/model';
+@Table({
+  timestamps: true,
+})
+
+export class UserRoles extends Model<IUserRoles>{
+  @ForeignKey(() => UserRoles)
+  @Column({ type: DataTypes.INTEGER, primaryKey: true })
+  id: number;
+
+  @Column({ type: DataTypes.INTEGER })
+  user_id: number;
+
+  @Column({ type: DataTypes.INTEGER })
+  role_id: number;
+  
+}
