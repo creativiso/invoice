240f400836873e2443f65f5021bddc45dc6ee367
Medina <114340934+MedinaAhmed@users.noreply.github.com>
2023-02-13 15:47:52 +0200

    adding proformasForm (#20)

--- apps/frontend/src/app/modules/proformas/proformas.module.ts
+++ apps/frontend/src/app/modules/proformas/proformas.module.ts
@@ -2,19 +2,42 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ProformasComponent } from './proformas.component';
-
-
-const routes: Routes = [
-  { path: '', component: ProformasComponent }
-];
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
+const routes: Routes = [{ path: '', component: ProformasComponent }];
 
 @NgModule({
-  declarations: [
-    ProformasComponent
-  ],
+  declarations: [ProformasComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
-  ]
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatCardModule,
+    MatRadioModule,
+    MatTableModule,
+    FlexLayoutModule,
+    RouterModule.forChild(routes),
+  ],
 })
-export class ProformasModule { }
+export class ProformasModule {}
