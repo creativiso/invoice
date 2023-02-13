ae37aca70be0846a10e04c6f125b58cc041ca348
Medina Ahmed <meditotrampova@gmail.com>
2023-02-13 15:31:58 +0200

    adding proformasForm

--- apps/frontend/src/app/modules/proformas/proformas.component.ts
+++ apps/frontend/src/app/modules/proformas/proformas.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+interface SelectedMeasure {
+  [id: number]: string;
+}
 @Component({
   selector: 'crtvs-proformas',
   templateUrl: './proformas.component.html',
-  styleUrls: ['./proformas.component.css']
+  styleUrls: ['./proformas.component.scss'],
 })
 export class ProformasComponent implements OnInit {
+  proformasForm: FormGroup;
+  selectedMeasure: SelectedMeasure = {};
+  selectedCurrency = '';
+  quantity = 0.0;
+  priceWithoutVat = 0.0;
+  vatPercent = 0;
 
-  constructor() { }
+  tableData: {
+    nameField: string;
+    quantity: number;
+    measure: string;
+    priceWithoutVat: number;
+    value: number;
+  }[] = [
+    {
+      nameField: '',
+      quantity: 0,
+      measure: '',
+      priceWithoutVat: 0.0,
+      value: 0.0,
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.proformasForm = new FormGroup({
+      supplierName: new FormControl('', Validators.required),
+      supplierEik: new FormControl('', Validators.required),
+      supplierVatNumber: new FormControl(''),
+      supplierManager: new FormControl('', Validators.required),
+      supplierCity: new FormControl('', Validators.required),
+      supplierAddress: new FormControl('', Validators.required),
+      receiverName: new FormControl('', Validators.required),
+      individualPerson: new FormControl(false),
+      receiverEik: new FormControl('', Validators.required),
+      receiverVatNumber: new FormControl(''),
+      receiverManager: new FormControl('', Validators.required),
+      receiverCity: new FormControl('', Validators.required),
+      receiverAddress: new FormControl('', Validators.required),
+      releasedAt: new FormControl('', Validators.required), //datepicker
+      currency: new FormControl(''),
+      nameField: new FormControl('', Validators.required),
+      quantity: new FormControl('', Validators.required),
+      measure: new FormControl(''),
+      priceWithoutVat: new FormControl('', Validators.required),
+      vatPercent: new FormControl('', Validators.required),
+      vatReason: new FormControl(''),
+      wayOfPaying: new FormControl('', Validators.required),
+    });
   }
-
+  ngOnInit() {
+    this.proformasForm.get('vatPercent')?.valueChanges.subscribe((value) => {
+      this.vatPercent = value;
+    });
+    this.proformasForm.get('quantity')?.valueChanges.subscribe((value) => {
+      this.quantity = value;
+    });
+    this.proformasForm
+      .get('priceWithoutVat')
+      ?.valueChanges.subscribe((value) => {
+        this.priceWithoutVat = value;
+      });
+  }
+  
 }
