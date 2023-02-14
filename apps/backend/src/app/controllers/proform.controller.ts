027b60c45008c51e6eb8f0133d114c936607190a
Medina Ahmed <meditotrampova@gmail.com>
2023-02-14 09:40:37 +0200

    fixing errror

--- apps/backend/src/app/controllers/proform.controller.ts
+++ apps/backend/src/app/controllers/proform.controller.ts
@@ -1,153 +1,55 @@
-import { Router } from 'express';
-import { ProformService } from '../services/proform.service';
+import { Request, Response, Router } from 'express';
+import {
+  createProform,
+  getAllProforms,
+  getProformById,
+  updateProform,
+} from '../services/proform.service';
 
-export const proformsRouter = Router();
-const proformService = new ProformService();
-proformsRouter.get('/', async (req, res) => {
-  const proform = await proformService.getAllProforms();
-  res.json(proform);
-});
+export const proformRouter = Router();
 
-proformsRouter.get('/:id', async (req, res) => {
+// Create user
+proformRouter.post('/', async (req: Request, res: Response) => {
   try {
-    const id = Number(req.params.id);
-    const proform = await proformService.getProformById(id);
-    res.json(proform);
+    const newUser = await createProform(req.body);
+    res.json(newUser);
   } catch (error) {
-    res.status(404).json({ error: 'Proform not found' });
+    console.error(error);
+    res.status(500).send('Error creating user');
   }
 });
-//create
-proformsRouter.post('/', async (req, res) => {
-  const {
-    contractor,
-    issue_date,
-    bank_payment,
-    vat,
-    novatreason,
-    currency,
-    rate,
-    c_name,
-    c_city,
-    c_address,
-    c_eik,
-    c_ddsnumber,
-    c_mol,
-    c_person,
-    c_egn,
-    p_name,
-    p_city,
-    p_address,
-    p_eik,
-    p_ddsnumber,
-    p_mol,
-    p_bank,
-    p_iban,
-    p_bic,
-    p_zdds,
-    author,
-    author_user,
-    author_sign,
-  } = req.body;
-  const proform = await proformService.createProform(
-    contractor,
-    issue_date,
-    bank_payment,
-    vat,
-    novatreason,
-    currency,
-    rate,
-    c_name,
-    c_city,
-    c_address,
-    c_eik,
-    c_ddsnumber,
-    c_mol,
-    c_person,
-    c_egn,
-    p_name,
-    p_city,
-    p_address,
-    p_eik,
-    p_ddsnumber,
-    p_mol,
-    p_bank,
-    p_iban,
-    p_bic,
-    p_zdds,
-    author,
-    author_user,
-    author_sign
-  );
-  res.json(proform);
-});
-//update
-proformsRouter.put('/:id', async (req, res) => {
+// Get all users
+proformRouter.get('/', async (req: Request, res: Response) => {
   try {
-    const id = Number(req.params.id);
-    const {
-      contractor,
-      issue_date,
-      bank_payment,
-      vat,
-      novatreason,
-      currency,
-      rate,
-      c_name,
-      c_city,
-      c_address,
-      c_eik,
-      c_ddsnumber,
-      c_mol,
-      c_person,
-      c_egn,
-      p_name,
-      p_city,
-      p_address,
-      p_eik,
-      p_ddsnumber,
-      p_mol,
-      p_bank,
-      p_iban,
-      p_bic,
-      p_zdds,
-      author,
-      author_user,
-      author_sign,
-    } = req.body;
-    const proform = await proformService.updateProform(
-      id,
-      contractor,
-      issue_date,
-      bank_payment,
-      vat,
-      novatreason,
-      currency,
-      rate,
-      c_name,
-      c_city,
-      c_address,
-      c_eik,
-      c_ddsnumber,
-      c_mol,
-      c_person,
-      c_egn,
-      p_name,
-      p_city,
-      p_address,
-      p_eik,
-      p_ddsnumber,
-      p_mol,
-      p_bank,
-      p_iban,
-      p_bic,
-      p_zdds,
-      author,
-      author_user,
-      author_sign
-    );
-    res.json(proform);
+    const users = await getAllProforms();
+    res.json(users);
   } catch (error) {
-    res.status(404).json({ error: 'Proforms not found' });
+    console.error(error);
+    res.status(500).send('Error getting proforms');
   }
 });
+
+// Get a single user by ID
+proformRouter.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const user = await getProformById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
+
+// Update a user by ID
+proformRouter.put('/:id', async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const updatedUser = await updateProform(id, req.body);
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error updating proform');
+  }
+});
+
+export default proformRouter;
--- apps/backend/src/app/services/proform.service.ts
+++ apps/backend/src/app/services/proform.service.ts
@@ -1,144 +1,47 @@
 import { Proform } from '../model/models/Proform';
 
-export class ProformService {
-  async getAllProforms() {
-    return Proform.findAll();
+// Create user
+export async function createProform(proformData) {
+  try {
+    const newProform = await Proform.create(proformData);
+    return newProform;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error creating user');
   }
+}
+// Get all users
+export async function getAllProforms() {
+  return await Proform.findAll();
+}
 
-  async getProformById(id: number) {
+// Get a single user by ID
+export async function getProformById(id) {
+  try {
     const proform = await Proform.findByPk(id);
     if (!proform) {
       throw new Error('Proform not found');
     }
     return proform;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error getting proform');
   }
-
-  async createProform(
-    contractor: number,
-    issue_date: Date,
-    bank_payment: number,
-    vat: number,
-    novatreason: string,
-    currency: number,
-    rate: number,
-    c_name: string,
-    c_city: string,
-    c_address: string,
-    c_eik: string,
-    c_ddsnumber: string,
-    c_mol: string,
-    c_person: string,
-    c_egn: string,
-    p_name: string,
-    p_city: string,
-    p_address: string,
-    p_eik: string,
-    p_ddsnumber: string,
-    p_mol: string,
-    p_bank: string,
-    p_iban: string,
-    p_bic: string,
-    p_zdds: boolean,
-    author: string,
-    author_user: number,
-    author_sign: string
-  ) {
-    return Proform.create({
-      contractor,
-      issue_date,
-      bank_payment,
-      vat,
-      novatreason,
-      currency,
-      rate,
-      c_name,
-      c_city,
-      c_address,
-      c_eik,
-      c_ddsnumber,
-      c_mol,
-      c_person,
-      c_egn,
-      p_name,
-      p_city,
-      p_address,
-      p_eik,
-      p_ddsnumber,
-      p_mol,
-      p_bank,
-      p_iban,
-      p_bic,
-      p_zdds,
-      author,
-      author_user,
-      author_sign,
+}
+// Update a user by ID
+export async function updateProform(id, body) {
+  try {
+    const [affectedCount, affectedRows] = await Proform.update(body, {
+      where: { id },
+      returning: true,
     });
-  }
-
-  async updateProform(
-    id: number,
-    contractor: number,
-    issue_date: Date,
-    bank_payment: number,
-    vat: number,
-    novatreason: string,
-    currency: number,
-    rate: number,
-    c_name: string,
-    c_city: string,
-    c_address: string,
-    c_eik: string,
-    c_ddsnumber: string,
-    c_mol: string,
-    c_person: string,
-    c_egn: string,
-    p_name: string,
-    p_city: string,
-    p_address: string,
-    p_eik: string,
-    p_ddsnumber: string,
-    p_mol: string,
-    p_bank: string,
-    p_iban: string,
-    p_bic: string,
-    p_zdds: boolean,
-    author: string,
-    author_user: number,
-    author_sign: string
-  ) {
-    const proform = await Proform.findByPk(id);
-    if (!proform) {
-      throw new Error('Proform not found');
+    if (affectedCount === 0) {
+      return { status: 404, message: 'Proform not found' };
+    } else {
+      return { status: 200, data: affectedRows[0] };
     }
-    await proform.update({
-      contractor,
-      issue_date,
-      bank_payment,
-      vat,
-      novatreason,
-      currency,
-      rate,
-      c_name,
-      c_city,
-      c_address,
-      c_eik,
-      c_ddsnumber,
-      c_mol,
-      c_person,
-      c_egn,
-      p_name,
-      p_city,
-      p_address,
-      p_eik,
-      p_ddsnumber,
-      p_mol,
-      p_bank,
-      p_iban,
-      p_bic,
-      p_zdds,
-      author,
-      author_user,
-      author_sign,
-    });
+  } catch (error) {
+    console.error(error);
+    return { status: 500, message: 'Proform updating user' };
   }
 }
