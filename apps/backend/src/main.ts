50d959f237677e5fd224d70849c554182fe8b1a5
Medina <114340934+MedinaAhmed@users.noreply.github.com>
2023-02-20 11:56:30 +0200

    Ci 12 (#23)
    * adding controller and service for proforms
    
    * fixing errror
    
    * фиь буг
    
    * updating controller and service
    
    * updating
    
    * removig console log
    
    * fixing bug

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { sequelize } from './app/model/index';
-import { usersRouter } from './app/controllers/user.controller'
+import { usersRouter } from './app/controllers/user.controller';
 import { Router } from 'express';
 import { contractorsRouter } from '../src/app/controllers/contractor.controller';
+import { proformsRouter } from './app/controllers/proform.controller';
 
 export const apiRouter = Router();
 (async () => {
@@ -17,7 +18,8 @@ export const apiRouter = Router();
   const apiRouter = Router();
   app.use('/api/v1', apiRouter);
   apiRouter.use('/users', usersRouter);
-  apiRouter.use('/contractors', contractorsRouter);
+  app.use('/api/v1/contractors', contractorsRouter);
+  apiRouter.use('/proforms', proformsRouter);
   const port = process.env.port || 3333;
   const server = app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/api`);
