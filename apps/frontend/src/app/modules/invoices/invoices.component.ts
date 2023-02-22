d7a5eb1ccc423a886186c5f70b6b50999beebfa9
DenisZhelev <Deniszhelev09@gmail.com>
2023-02-22 11:16:09 +0200

    Invoices interface

--- apps/frontend/src/app/modules/invoices/invoices.component.ts
+++ apps/frontend/src/app/modules/invoices/invoices.component.ts
@@ -1,15 +1,115 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
+interface SelectedMeasure {
+  [id: number]: string;
+}
 @Component({
   selector: 'crtvs-invoices',
   templateUrl: './invoices.component.html',
-  styleUrls: ['./invoices.component.css']
+  styleUrls: ['./invoices.component.scss']
 })
 export class InvoicesComponent implements OnInit {
+  invoicesForm: FormGroup;
+  selectedMeasure: SelectedMeasure = {};
+  selectedCurrency = '';
+  quantity = 0.0;
+  priceWithoutVat = 0.0;
+  vatPercent = 0;
 
-  constructor() { }
+  tableData: {
+    nameField: string;
+    quantity: number;
+    measure: string;
+    priceWithoutVat: number;
+    value: number;
+  }[] = [
+    {
+      nameField: '',
+      quantity: 0,
+      measure: '',
+      priceWithoutVat: 0.0,
+      value: 0.0,
+    },
+  ];
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.invoicesForm = new FormGroup({
+      supplierName: new FormControl('', Validators.required),
+      supplierEik: new FormControl('', Validators.required),
+      supplierVatNumber: new FormControl(''),
+      supplierManager: new FormControl('', Validators.required),
+      supplierCity: new FormControl('', Validators.required),
+      supplierAddress: new FormControl('', Validators.required),
+      receiverName: new FormControl('', Validators.required),
+      individualPerson: new FormControl(false),
+      receiverEik: new FormControl('', Validators.required),
+      receiverVatNumber: new FormControl(''),
+      receiverManager: new FormControl('', Validators.required),
+      receiverCity: new FormControl('', Validators.required),
+      receiverAddress: new FormControl('', Validators.required),
+      typeOfInvoice: new FormControl('', Validators.required),
+      issuedAt: new FormControl('', Validators.required), //datepicker
+      eventAt: new FormControl('', Validators.required), //datepicker
+      currency: new FormControl(''),
+      nameField: new FormControl('', Validators.required),
+      quantity: new FormControl('', Validators.required),
+      measure: new FormControl(''),
+      priceWithoutVat: new FormControl('', Validators.required),
+      vatPercent: new FormControl('', Validators.required),
+      vatReason: new FormControl(''),
+      wayOfPaying: new FormControl('', Validators.required),
+    })
+   }
+   
+
+  ngOnInit(){
+    this.invoicesForm.get('vatPercent')?.valueChanges.subscribe((value) => {
+      this.vatPercent = value;
+    });
+    this.invoicesForm.get('quantity')?.valueChanges.subscribe((value) => {
+      this.quantity = value;
+    });
+    this.invoicesForm
+      .get('priceWithoutVat')
+      ?.valueChanges.subscribe((value) => {
+        this.priceWithoutVat = value;
+      });
+  }
+  //geting the sum of quntity and unitPrice
+  get amount(): number {
+    return this.quantity * this.priceWithoutVat;
+  }
+  get totalAmount(): number {
+    return (this.amount * this.vatPercent) / 100 + this.amount;
+  }
+  addRow() {
+    const newRow = {
+      nameField: '',
+      quantity: 0,
+      measure: '',
+      priceWithoutVat: 0.0,
+      value: 0.0,
+    };
+    this.tableData.push(newRow);
+    this.selectedMeasure[this.tableData.length - 1] = '';
   }
 
+  deleteRow(index: number) {
+    if (this.tableData.length > 1) {
+      this.tableData.splice(index, 1);
+    } else {
+      // code to show an error message or alert
+      console.log('You should have at least one row');
+    }
+  }
+
+  onSubmit() {
+    //
+  }
 }
