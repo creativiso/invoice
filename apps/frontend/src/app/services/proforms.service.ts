8b9ec2847ea9ec8c33d6ce5ab99bc311b21af3bc
Medina Ahmed <meditotrampova@gmail.com>
2023-03-01 20:51:24 +0200

    adding update method to service and read

--- apps/frontend/src/app/services/proforms.service.ts
+++ apps/frontend/src/app/services/proforms.service.ts
@@ -21,4 +21,26 @@ export class ProformsService {
     };
     return this.http.post(`${this.apiUrl}/add`, data);
   }
+  updateProform(
+    proformId: number,
+    proformData: IProform,
+    items: IProformItem[]
+  ) {
+    const proformItems = items.map((item) => ({
+      ...item,
+      proform: proformData.id,
+    }));
+    const data = {
+      ...proformData,
+      items: proformItems,
+    };
+    return this.http.put(`${this.apiUrl}/${proformId}`, data);
+  }
+  getAllProforms() {
+    return this.http.get(`${this.apiUrl}/`);
+  }
+
+  getProformById(proformId: number) {
+    return this.http.get(`${this.apiUrl}/${proformId}`);
+  }
 }
