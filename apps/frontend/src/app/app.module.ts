01ec21f9a5726ea498a5bbc205a8779947a6f734
Medina Ahmed <meditotrampova@gmail.com>
2023-03-09 21:58:41 +0200

    adding login module

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -56,7 +56,18 @@ import { MatIconModule } from '@angular/material/icon';
               (m) => m.ProfileModule
             ),
         },
-      { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+        {
+          path: 'login',
+          loadChildren: () =>
+            import('./modules/login/login.module').then((m) => m.LoginModule),
+        },
+        {
+          path: 'dashboard',
+          loadChildren: () =>
+            import('./modules/dashboard/dashboard.module').then(
+              (m) => m.DashboardModule
+            ),
+        },
       ],
       { initialNavigation: 'enabledBlocking' }
     ),
---
+++ apps/frontend/src/app/modules/login/login.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+const routes: Routes = [{ path: '', component: LoginComponent }];
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forChild(routes),
+  ],
+})
+export class LoginModule {}
