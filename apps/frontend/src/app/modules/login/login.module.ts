68a9a2bc33b654a78bf76d13e4b7526536657d4a
Medina Ahmed <meditotrampova@gmail.com>
2023-03-14 07:35:11 +0200

    addidn design to form

--- apps/frontend/src/app/modules/login/login.module.ts
+++ apps/frontend/src/app/modules/login/login.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
@@ -19,6 +20,7 @@ const routes: Routes = [{ path: '', component: LoginComponent }];
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
     RouterModule.forChild(routes),
   ],
 })
