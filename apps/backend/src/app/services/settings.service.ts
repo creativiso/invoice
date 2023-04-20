db0e0f395657c52f1ab2d6cdb48a106ea7240e49
DenisZhelev <Deniszhelev09@gmail.com>
2023-04-20 21:31:11 +0300

    Setting create if empty

--- apps/backend/src/app/services/settings.service.ts
+++ apps/backend/src/app/services/settings.service.ts
@@ -1,10 +1,38 @@
 import { Settings } from '../model/models/Settings';
+import { ISettings} from '../../../../../libs/typings/src/index'
 
 export class SettingsService {
   async getSettings() {
     const id =1;
-    return await Settings.findByPk(id);
+    const setting =  await Settings.findByPk(id);
+    if(!setting)
+    {
+      const defaultSettings: ISettings = {
+        supplierName: '',
+        supplierVatNumber: '',
+        supplierCity: '',
+        supplierAddress: '',
+        iban: '',
+        bicSwift: '',
+        bank: '',
+        dds: false,
+        paymentMethod: '',
+        individualPerson: false,
+        quantityNumber: 2,
+        singlePriceNumber: 2,
+        totalPriceNumber: 2,
+        supplierEik: '',
+        supplierManager: '',
+        units: []
+      };
+  
+      // create the new settings object in the database
+      const newSettings = await Settings.create(defaultSettings);
+  
+      return newSettings;
+    }
   }
+
   async createSetting(data: Settings): Promise<Settings> {
     return Settings.create(data);
   }
@@ -26,3 +54,4 @@ async deleteSetting(id: number): Promise<boolean> {
   return true;
 }
 }
+
