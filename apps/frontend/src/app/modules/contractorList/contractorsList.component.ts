21809d0e30481311f3f5cbe1c84eef7687319f71
Medina Ahmed <meditotrampova@gmail.com>
2023-05-11 12:21:53 +0300

    aading paginator

--- apps/frontend/src/app/modules/contractorList/contractorsList.component.ts
+++ apps/frontend/src/app/modules/contractorList/contractorsList.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ContractorsService } from 'src/app/services/contractors.service';
 import { IContractor } from '../../../../../../libs/typings/src';
 import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'crtvs-contractors-list',
@@ -9,10 +12,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./contractorsList.component.scss'],
 })
 export class ContractorListComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   displayedColumns: string[] = ['name', 'city', 'egn', 'mol', 'tools'];
 
   contractors: IContractor[] = [];
-
+  dataSource = new MatTableDataSource<IContractor>(this.contractors);
   constructor(
     private router: Router,
     private contractorsService: ContractorsService
@@ -21,6 +26,7 @@ export class ContractorListComponent implements OnInit {
     this.contractorsService.getAllContractors().subscribe({
       next: (data: IContractor[]) => {
         this.contractors = data;
+        this.dataSource.data = this.contractors;
       },
       error: (error) => {
         console.error(error);
@@ -35,5 +41,8 @@ export class ContractorListComponent implements OnInit {
     this.router.navigate(['/contractors', contractor.id, 'edit'], {
       state: { data: contractor },
     });
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
--- apps/frontend/src/app/modules/contractorList/contractorsList.module.ts
+++ apps/frontend/src/app/modules/contractorList/contractorsList.module.ts
@@ -14,6 +14,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 const routes: Routes = [{ path: '', component: ContractorListComponent }];
 
@@ -34,6 +35,7 @@ const routes: Routes = [{ path: '', component: ContractorListComponent }];
     MatNativeDateModule,
     MatTableModule,
     MatTooltipModule,
+    MatPaginatorModule,
   ],
 })
 export class ContractorsListModule {}
