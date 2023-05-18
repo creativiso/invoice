6e93ccec770bc667890bacde8ad5e6dc503a90d7
Medina Ahmed <meditotrampova@gmail.com>
2023-05-18 13:11:30 +0300

    add pagnator

--- apps/frontend/src/app/modules/invoicesList/invoicesList.module.ts
+++ apps/frontend/src/app/modules/invoicesList/invoicesList.module.ts
@@ -14,6 +14,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 const routes: Routes = [{ path: '', component: InvoicesListComponent }];
 
@@ -34,6 +35,7 @@ const routes: Routes = [{ path: '', component: InvoicesListComponent }];
     MatNativeDateModule,
     MatTableModule,
     MatTooltipModule,
+    MatPaginatorModule,
   ],
 })
 export class InvoicesListModule {}
