3b60cd177d4f83cb7112bce72df264dbc059dc2d
ZumaLux <zumalux99@gmail.com>
2024-01-30 01:42:13 +0200

    Proformas module refactored

---
+++ apps/frontend/src/app/modules/proformas/proforma/proforma.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProformaComponent } from './proforma.component';
+
+describe('ProformaComponent', () => {
+  let component: ProformaComponent;
+  let fixture: ComponentFixture<ProformaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProformaComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ProformaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
