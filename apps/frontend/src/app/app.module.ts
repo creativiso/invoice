12a3737bb41e11c95a68f4c3644da21485c33d32
ZumaLux <zumalux99@gmail.com>
2024-01-30 11:15:01 +0200

    Routes moved to appRouting.module.ts

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -13,86 +12,18 @@ import { AuthGuard } from './auth/auth.guard';
 import { AuthService } from './services/auth.service';
 import { NotLoggedInGuard } from './auth/notLoggedIn.guard';
 import { MatRippleModule } from '@angular/material/core';
+import { AppRoutingModule } from './appRouting.module';
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, NavigationComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      [
-        {
-          path: 'invoices',
-          loadChildren: () =>
-            import('./modules/invoices/invoices.module').then(
-              (m) => m.InvoicesModule
-            ),
-          canActivate: [AuthGuard],
-        },
-        {
-          path: 'proformas',
-          loadChildren: () =>
-            import('./modules/proformas/proformas.module').then(
-              (m) => m.ProformasModule
-            ),
-          canActivate: [AuthGuard],
-        },
-        {
-          path: 'contractors',
-          loadChildren: () =>
-            import('./modules/contractors/contractors.module').then(
-              (m) => m.ContractorsModule
-            ),
-          canActivate: [AuthGuard],
-        },
-        {
-          path: 'settings',
-          loadChildren: () =>
-            import('./modules/settings/settings.module').then(
-              (m) => m.SettingsModule
-            ),
-          canActivate: [AuthGuard],
-        },
-        {
-          path: 'users',
-          loadChildren: () =>
-            import('./modules/users/users.module').then((m) => m.UsersModule),
-          canActivate: [AuthGuard],
-        },
-        {
-          path: 'profile',
-          loadChildren: () =>
-            import('./modules/profile/profile.module').then(
-              (m) => m.ProfileModule
-            ),
-          canActivate: [AuthGuard],
-        },
-        {
-          path: 'login',
-          loadChildren: () =>
-            import('./modules/login/login.module').then((m) => m.LoginModule),
-          canActivate: [NotLoggedInGuard],
-        },
-        {
-          path: 'dashboard',
-          loadChildren: () =>
-            import('./modules/dashboard/dashboard.module').then(
-              (m) => m.DashboardModule
-            ),
-          canActivate: [AuthGuard],
-        },
-        {
-          path: '',
-          pathMatch: 'full',
-          redirectTo: 'dashboard',
-        },
-      ],
-      { initialNavigation: 'enabledBlocking' }
-    ),
     BrowserAnimationsModule,
     MaterialModule,
     MatMenuModule,
     MatIconModule,
     MatRippleModule,
     HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [AuthService, AuthGuard, NotLoggedInGuard],
   bootstrap: [AppComponent],
