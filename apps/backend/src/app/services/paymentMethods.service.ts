4a88c68e1a728bd408699cf487263e8cc31cf17f
ZumaLux <zumalux99@gmail.com>
2024-02-06 04:56:16 +0200

    Duplicated files deleted

--- apps/backend/src/app/services/paymentMethods.service.ts
+++
@@ -1,8 +0,0 @@
-import { PaymentMethod } from '../model';
-
-export class PaymentMethodsService {
-  // Get all payment methods
-  async getAllPaymentMethods() {
-    return PaymentMethod.findAll();
-  }
-}
